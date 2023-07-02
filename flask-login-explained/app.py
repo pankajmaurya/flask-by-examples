b89d3e77393e4ff6137fba420ea780752f7305a6
Pankaj Maurya <pankajmaurya@gmail.com>
2023-07-03 00:16:56 +0530

    enhanced the example with ability to also do registration of user and have an error page too

--- flask-login-explained/app.py
+++ flask-login-explained/app.py
@@ -11,15 +11,23 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # Create a simple User model for demonstration
+# class User(UserMixin):
+#     def __init__(self, user_id):
+#         self.id = user_id
+#         self.name = "User" + str(self.id)
+
+# Create a simple User model for demonstration
 class User(UserMixin):
-    def __init__(self, user_id):
+    def __init__(self, user_id, username, password):
         self.id = user_id
+        self.username = username
+        self.password = password
         self.name = "User" + str(self.id)
 
 # Dummy user database for demonstration
 users = {
-    '1': User('1'),
-    '2': User('2')
+    '1': User('1', 'john', 'password'),
+    '2': User('2', 'jane', 'password')
 }
 
 # User loader function required by Flask-Login
@@ -34,20 +42,23 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user_id = request.form.get('user_id')
-        user = users.get(user_id)
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = next((user for user in users.values() if user.username == username and user.password == password), None)
         if user:
             login_user(user)
             return redirect(url_for('dashboard'))
         else:
-            return 'Invalid user ID'
+            return render_template('error.html', error_message='Invalid username or password')
     else:
         return render_template('login.html')
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', user_id=current_user.id)
+    return render_template('dashboard.html', user_id=current_user.id, username=current_user.username)
+
 
 @app.route('/logout')
 @login_required
@@ -55,5 +66,23 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if any(user.username == username for user in users.values()):
+            return render_template('error.html', error_message='Username already exists')
+
+        user_id = str(len(users) + 1)
+        user = User(user_id, username, password)
+        users[user_id] = user
+
+        login_user(user)
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('register.html')
+    
 if __name__ == '__main__':
     app.run()
